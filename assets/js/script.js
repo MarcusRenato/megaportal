719020000900ee35481ab87253c8c406c79c50dd
Marcus Renato <marcusrenato@live.com>
2019-07-09 11:25:54 -0300

    Adicionando responsividade no site

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,39 @@
 var slideItem = 0;
 window.onload = function() {
-	setInterval(passarSlide, 2000);
+    setInterval(passarSlide, 2000);
 
-	var slidewidth = document.getElementById("slideshow").offsetWidth;
-	var objs = document.getElementsByClassName("slide");
-	for(var i=0;i<objs.length;i++) {
-		objs[i].style.width = slidewidth+"px";
-	}
+    var slidewidth = document.getElementById("slideshow").offsetWidth;
+    var objs = document.getElementsByClassName("slide");
+    for (var i = 0; i < objs.length; i++) {
+        objs[i].style.width = slidewidth + "px";
+    }
 }
+
 function passarSlide() {
-	var slidewidth = document.getElementById("slideshow").offsetWidth;
-	
-	if(slideItem >= 3) {
-		slideItem = 0;
-	} else {
-		slideItem++;
-	}
+    var slidewidth = document.getElementById("slideshow").offsetWidth;
 
-	document.getElementsByClassName("slideshowarea")[0].style.marginLeft = "-"+(slidewidth * slideItem)+"px";
+    if (slideItem >= 3) {
+        slideItem = 0;
+    } else {
+        slideItem++;
+    }
+
+    document.getElementsByClassName("slideshowarea")[0].style.marginLeft = "-" + (slidewidth * slideItem) + "px";
 }
+
 function mudarSlide(pos) {
-	slideItem = pos;
-	var slidewidth = document.getElementById("slideshow").offsetWidth;
-	document.getElementsByClassName("slideshowarea")[0].style.marginLeft = "-"+(slidewidth * slideItem)+"px";
+    slideItem = pos;
+    var slidewidth = document.getElementById("slideshow").offsetWidth;
+    document.getElementsByClassName("slideshowarea")[0].style.marginLeft = "-" + (slidewidth * slideItem) + "px";
 }
 
 function toggleMenu() {
 
-	var menu = document.getElementById("menu");
+    var menu = document.getElementById("menu");
 
-	if (menu.style.display == 'none' || menu.style.display == '') {
-		menu.style.display = "block";
-	} else {
-		menu.style.display = "none";
-	}
+    if (menu.style.display == 'none' || menu.style.display == '') {
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
 }
